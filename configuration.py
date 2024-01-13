05a041bb43173f8586751752f7bddf1558934d2c
Elias <eliasislasleyva87@gmail.com>
2024-01-13 00:28:12 -0600

    creacion de usuario y obtencion de Token

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE ="https://ab68cfc7-47e5-4942-85a5-efbc89fd861d.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+CREATE_USER_PATH ="/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
+KITS_PATH ="/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
