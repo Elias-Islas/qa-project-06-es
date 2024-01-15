43424b5ebbd276d1829173fd30c83773fe8a213c
Elias <eliasislasleyva87@gmail.com>
2024-01-14 18:30:15 -0600

    el token se obtiene en una funcion get_token

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,6 +1,10 @@
 import sender_stand_request
 import data
 
+def get_token():
+    response = sender_stand_request.post_new_user(data.user_body)
+    return response.json()["authToken"]
+
 def get_headers_kits(token):
     current_headers = data.headers_kits.copy()
     current_headers["Authorization"] = "Bearer " + token
@@ -38,61 +42,43 @@ def negative_assert_without_name(token):
 #Prueba 1. Creación de un nuevo usuario o usuaria
 #El parámetro "firstName" contiene dos caracteres
 def test_create_kit_1_letter_in_name_get_success_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
-    positive_assert("a", auth_token)
+    positive_assert("a", get_token())
 
 #prueba 2
 def test_create_kit_511_letter_in_name_get_success_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
     name = "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"
-    positive_assert(name, auth_token)
+    positive_assert(name, get_token())
 
 #prueba 3
 def test_create_user_0_letter_in_name_get_error_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
     name = ""
-    negative_assert_symbol(name, auth_token)
+    negative_assert_symbol(name, get_token())
 #prueba 4
 def test_create_user_512_letter_in_name_get_error_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
     name = "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcD"
-    negative_assert_symbol(name, auth_token)
+    negative_assert_symbol(name, get_token())
 #prueba 5
 def test_create_kit_has_special_symbol_in_name_get_success_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
     name = "\"№%@\","
-    positive_assert(name, auth_token)
+    positive_assert(name, get_token())
 
 #prueba 6
 def test_create_kit_has_space_in_name_get_success_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
     name = "A Aaa"
-    positive_assert(name, auth_token)
+    positive_assert(name, get_token())
 
 #prueba 7
 def test_create_kit_has_number_in_name_get_success_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
     name = "123"
-    positive_assert(name, auth_token)
+    positive_assert(name, get_token())
 
 #prueba 8
 def test_create_user_without_name_get_error_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
-    negative_assert_without_name(auth_token)
+    negative_assert_without_name(get_token())
 
 #prueba 9
 def test_create_user_has_number_in_name_get_error_response():
-    response = sender_stand_request.post_new_user(data.user_body)
-    auth_token = response.json()["authToken"]
     name = 123
-    negative_assert_symbol(name, auth_token)
+    negative_assert_symbol(name, get_token())
 
 
