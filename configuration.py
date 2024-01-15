175e81b7a1b6fde713d5013a93ca6a0ff22b5042
Elias-Islas <153335799+Elias-Islas@users.noreply.github.com>
2024-01-15 16:54:54 -0600

    Update configuration.py
    actualización del server

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE ="https://2df2f985-5a1d-4286-a50f-42e69d58a997.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE ="https://5db46c5f-e1cb-4469-9eec-0c9225d0cccb.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH ="/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH ="/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
