1337e47b0866b1e392260c02c763a727cfcfda22
Elias <eliasislasleyva87@gmail.com>
2024-01-14 20:01:50 -0600

    primer entrega proyecto

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE ="https://8668b265-6e3e-4a35-9d7e-f0540cde7633.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE ="https://2df2f985-5a1d-4286-a50f-42e69d58a997.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH ="/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH ="/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
