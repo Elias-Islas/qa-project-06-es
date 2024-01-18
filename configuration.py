98639d09d23d68ca14281a1a4ab3f34ecd603312
Elias <eliasislasleyva87@gmail.com>
2024-01-17 18:42:43 -0600

    organizacion y segundo intento de entrega sprint

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE ="https://140d38bf-5c15-42e2-b1ee-3e9a65289b72.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE ="https://5ec6ee92-50d2-4151-8447-6d482e87418e.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
 CREATE_USER_PATH ="/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH ="/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
